3ed4a7908f06f498f6797ddeb9c96610c5493a22
Matheus Bastani <matheusbastani4@gmail.com>
2025-12-11 03:49:25 -0300

    chore(root): setup monorepo with workspaces, husky, changesets, and linting

---
+++ .commitlintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat', // new feature
+        'fix', // bug fix
+        'chore', // changes to the build process or auxiliary tools
+        'docs', // changes to documentation
+        'style', // changes that do not affect the meaning of the code
+        'refactor', // a code change that neither fixes a bug nor adds a feature
+        'perf', // a code change that improves performance
+        'test' // adding missing tests
+      ]
+    ],
+
+    'scope-empty': [2, 'never'],
+    'scope-case': [2, 'always', 'lower-case'],
+    'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case']],
+    'subject-full-stop': [2, 'never', '.'],
+    'subject-min-length': [2, 'always', 10]
+  },
+
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^(\w+)\(([\w\-/]+)\):\s(.+)$/,
+      headerCorrespondence: ['type', 'scope', 'subject']
+    }
+  }
+}
