44de646e87f19688091a3036834f7bbe522d38c9
Matheus Bastani <matheusbastani4@gmail.com>
2025-12-21 16:56:20 -0300

    feat(scripts): add colored output in console

--- scripts/clean-node-modules.js
+++ scripts/clean-node-modules.js
@@ -2,11 +2,20 @@ import { rm } from 'fs/promises'
 import { readdir } from 'fs/promises'
 import { join } from 'path'
 
+const colors = {
+  reset: '\x1b[0m',
+  red: '\x1b[31m',
+  green: '\x1b[32m',
+  yellow: '\x1b[33m',
+  blue: '\x1b[34m'
+}
+
 async function cleanNodeModules(dir) {
   let files
   try {
     files = await readdir(dir, { withFileTypes: true })
   } catch (err) {
+    if (err.code !== 'ENOENT') throw err
     return
   }
 
@@ -16,9 +25,17 @@ async function cleanNodeModules(dir) {
     if (file.isDirectory()) {
       if (file.name === 'node_modules') {
         try {
-          console.log(`deleting ${fullPath}...`)
+          console.log(`${colors.blue}Deleting ${fullPath}...${colors.reset}`)
           await rm(fullPath, { recursive: true, force: true })
-        } catch (err) {}
+          console.log(`${colors.green}Deleted ${fullPath}${colors.reset}`)
+        } catch (err) {
+          if (err.code !== 'ENOENT') {
+            console.error(`${colors.red}Error deleting ${fullPath}: ${err}${colors.reset}`)
+            throw err
+          } else {
+            console.log(`${colors.yellow}Warning: ${fullPath} not found${colors.reset}`)
+          }
+        }
       } else {
         await cleanNodeModules(fullPath)
       }
@@ -27,5 +44,5 @@ async function cleanNodeModules(dir) {
 }
 
 cleanNodeModules(process.cwd())
-  .then(() => console.log('all node_modules deleted'))
-  .catch((err) => console.error('error:', err))
+  .then(() => console.log(`${colors.green}All node_modules deleted${colors.reset}`))
+  .catch((err) => console.error(`${colors.red}Error: ${err}${colors.reset}`))
