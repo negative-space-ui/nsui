a8b3ed25218d3d541e894f8f87b32c49b8c21d07
Matheus Bastani <matheusbastani4@gmail.com>
2026-01-21 03:30:20 -0300

    chore(primitive): remove Collection lib

--- packages/ui/primitive/collection/src/CollectionSeparator.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Divider, type DividerProps } from '@negative-space/divider'
-
-export type CollectionSeparatorProps = DividerProps
-
-export const CollectionSeparator = (props: CollectionSeparatorProps) => {
-  return <Divider role="separator" {...props} />
-}
