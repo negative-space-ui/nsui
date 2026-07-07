b58556419d140a5454beec92cbec1979a4133043
Matheus Bastani <matheusbastani4@gmail.com>
2026-07-07 10:59:05 -0300

    chore(provider): rename consts

--- packages/core/provider/src/styles/effects/spin.ts
+++ packages/core/provider/src/styles/effects/spin.ts
@@ -2,7 +2,7 @@ import { injectKeyframes, injectStyle } from '@negative-space/inject-css'
 
 import type { GlobalConfigRequired, SpinnerConfig } from '../../types'
 
-export const spinKeyframes = (global: GlobalConfigRequired, spinner: SpinnerConfig) => {
+export const spin = (global: GlobalConfigRequired, spinner: SpinnerConfig) => {
   injectKeyframes(`${global.prefixCls}-spin`, {
     '0%': { transform: 'rotate(0deg)' },
     '100%': { transform: 'rotate(360deg)' }
